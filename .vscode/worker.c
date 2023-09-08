f9754c94f14d54b368e13cd28225796a8308b5d1
hjht2d <hjht2d@umsystem.edu>
2023-09-08 12:58:15 -0500

    Wanted iteration to start at 1 not 0

--- .vscode/worker.c
+++ .vscode/worker.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
     pid_t pid = getpid();
     pid_t ppid = getppid();
 
-    for (int i = 0; i < num_loop_iter + 1; i++){
+    for (int i = 1; i < num_loop_iter + 1; i++){
         printf("WORKER PID:%d PPID:%d Iteration:%d before sleeping\n", pid, ppid, i);
         sleep(1);
         printf("WORKER PID:%d PPID:%d Iteration:%d after sleeping\n", pid, ppid,  i);
