38ae036c9accef34a385bb4f3c6bc57355b40bfc
hjht2d <hjht2d@umsystem.edu>
2023-09-08 12:24:38 -0500

    Added for loop for worker

--- .vscode/worker.c
+++ .vscode/worker.c
@@ -6,5 +6,9 @@ int main(int argc, char *argv[]){
     pid_t pid = getpid();
     pid_t ppid = getppid();
 
-    
+    for (int i = 0; i < num_loop_iter; i++){
+        printf("WORKER PID:%d PPID:%d Iteration:%d before sleeping\n", pid, ppid, i);
+        sleep(1);
+        printf("WORKER PID:%d PPID:%d Iteration:%d after sleeping\n", pid, ppid, i);
+    }
 }
