84e777d1a692e9b4bb02e9386c3737554d56068e
hjht2d <hjht2d@umsystem.edu>
2023-09-13 18:02:18 -0500

    added a help function and needed to convert string arguments to integer

--- .vscode/oss.c
+++ .vscode/oss.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void help(){
+        printf("Please use this template for running the program: oss [-h] [-n proc] [-s simul] [-t iter], Where -h is the help command, -n is the amount of children to be deployed, -s is the number of processes running at the same time and -t is the number of iterations worker.c will run. -n, -s, -t all take an integer\n");
+}
+
+
 int main(int argc, char *argv[]){
     int children, iterations, simulations, opt;
 
@@ -12,13 +18,13 @@ int main(int argc, char *argv[]){
                help(); 
                exit(0);
             case 'n':
-                children = optarg;
+                children = atoi(optarg);
                 break;
             case 's':
-                simulations = optarg;
+                simulations = atoi(optarg);
                 break;
             case 't':
-                iterations = optarg;
+                iterations = atoi(optarg);
                 break;
             default:
                 help();
