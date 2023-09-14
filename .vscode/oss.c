3fe351a81c2bdd5bf145acf09e0e595acc562c59
hjht2d <hjht2d@umsystem.edu>
2023-09-13 19:39:48 -0500

    A bit of a back track. I got rid of -s in getopt so that I could implement -n and -t which seem to be working

--- .vscode/oss.c
+++ .vscode/oss.c
@@ -2,7 +2,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/wait.h>
 
 void help(){
         printf("Please use this template for running the program: oss [-h] [-n proc] [-s simul] [-t iter], Where -h is the help command, -n is the amount of children to be deployed, -s is the number of processes running at the same time and -t is the number of iterations worker.c will run. -n, -s, -t all take an integer\n");
@@ -11,17 +11,15 @@ void help(){
 
 int main(int argc, char *argv[]){
     int children, iterations, simulations, opt;
+    char iter_to_string[3];
 
-    while ((opt = getopt(argc, argv, "hn:s:t:")) != -1){
+    while ((opt = getopt(argc, argv, "hn:t:")) != -1){
         switch(opt) {
             case 'h':
-               help(); 
+               help();
                exit(0);
             case 'n':
                 children = atoi(optarg);
-                break;
-            case 's':
-                simulations = atoi(optarg);
                 break;
             case 't':
                 iterations = atoi(optarg);
@@ -32,4 +30,22 @@ int main(int argc, char *argv[]){
         }
     }
 
+   sprintf(iter_to_string,"%d", iterations);
+
+   for (int i = 0; i < children; i++){
+          pid_t pid = fork();
+
+         if (pid == 0){
+                execlp("./worker", "worker", iter_to_string, NULL);
+                perror("execlp");
+                exit(1);
+         }
+   }
+
+   for (int i = 0; i < children; i++){
+           wait(0);
+   }
+
+   printf("All workers are done\n");
+   return 0;
 }
--- .vscode/worker.c
+++ .vscode/worker.c
@@ -5,12 +5,12 @@
 int main(int argc, char *argv[]){
     int num_loop_iter = atoi(argv[1]);
     pid_t pid = getpid();
-    //pid_t ppid = getppid();
+    pid_t ppid = getppid();
 
     for (int i = 1; i < num_loop_iter + 1; i++){
-        printf("WORKER PID:%d Iteration:%d before sleeping\n", pid, i);
+        printf("WORKER PID:%d PPID:%d Iteration:%d before sleeping\n", pid, ppid, i);
         sleep(1);
-        printf("WORKER PID:%d Iteration:%d after sleeping\n", pid, i);
+        printf("WORKER PID:%d PPID:%d Iteration:%d after sleeping\n", pid, ppid, i);
     }
 
     return 0;
